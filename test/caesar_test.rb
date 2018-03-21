d857f9e5d2c324fb634bca56a1598e938f935418
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-21 09:12:09 -0600

    initial commit

--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/caesar'
+
+class CaesarTest < Minitest::Test
+  attr_reader :caesar
+
+  def setup
+    @caesar = Caesar.new
+  end
+
+  def test_caesar_exists
+    assert_instance_of Caesar, caesar
+  end
+
+end
 # YOUR TESTS GOES HERE
 # I PITY THE FOOL WHO DOESNT WRITE TESTS
