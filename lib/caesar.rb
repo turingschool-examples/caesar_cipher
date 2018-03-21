fc4b9bd0cca16eb07ef6c5d73606bfd50686698d
Jude Lawson <lawsonjdutton@gmail.com>
2018-03-21 09:46:13 -0600

    All tests setup without errors, and are failing. Ready for methods

--- lib/caesar.rb
+++
@@ -1,6 +0,0 @@
-class Caesar
-  attr_reader :key
-
-  def initialize
-  end
-end
---
+++ lib/caeser.rb
@@ -0,0 +1,20 @@
+class Caeser
+
+  def initialize(shift_value)
+    @key = shift_value
+  end
+
+
+  def eng_to_cipher(phrase)
+  end 
+
+  def cipher_to_eng(cipher)
+  end
+
+  def from_file_to_cipher(input_file)
+  end
+
+  def from_file_to_eng(cipher_file)
+  end
+
+end
