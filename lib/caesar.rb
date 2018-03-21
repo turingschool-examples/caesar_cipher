7b070b397b8f9f7a212df5b00b6ecec99de032ab
Blythe McFall <mcfall.blythe@gmail.com>
2018-03-21 10:49:56 -0600

    Caesar Cypher - B. McFall - Partially Complete

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,42 @@
-class Caesar
-  attr_reader :key
+require 'pry'
 
-  def initialize
+class Caesar
+
+  attr_reader :caesar
+              :eng_to_cipher
+
+ def initialize
+  end
+
+  def eng_to_cipher_lowercase(string, shift = 3)
+    alphabet = Array('a'..'z')
+    decrypt = Hash[alphabet.zip(alphabet.rotate(shift))]
+    string.chars.map do |letter|
+      decrypt.fetch(letter, " ")
+    end
+  end
+
+  def eng_to_cipher_all_case(string, shift = 3)
+    alphabet = Array('A'..'z')
+    decrypt = Hash[alphabet.zip(alphabet.rotate(shift))]
+    string.chars.map do |letter|
+      decrypt.fetch(letter, " ")
+    end
+  end
+
+  def eng_to_cipher_from_file(shift = 3)
+    alphabet = Array('A'..'z')
+    decrypt = Hash[alphabet.zip(alphabet.rotate(shift))]
+    binding.pry
+    File.open("input.txt").chars.each do |letter|
+      decrypt.fetch(letter, " ")
+    end
   end
 end
+
+caesar = Caesar.new#("Gaius Julius Caesar")
+p caesar.eng_to_cipher_lowercase("the quick brown fox jumps over the lazy dog").join
+p caesar.eng_to_cipher_all_case("The quick brown fox JUMPS over the lazy DOG").join
+
+#I couldn't make this work; didn't get to it fast enough.
+#p eng_to_cipher_from_file.join
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,13 @@
 # YOUR TESTS GOES HERE
 # I PITY THE FOOL WHO DOESNT WRITE TESTS
+require "minitest/autorun"
+require "minitest/pride" #Color
+require 'pry'
+
+class Caesar < Minitest::Test
+
+  def test_it_exists
+    caesar = Caesar.new
+    assert_instance_of Caesar, caesar
+  end
+end
