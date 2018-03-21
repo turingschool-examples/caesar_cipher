43344cd9313ad6efb4ae62481002e895ee451684
brickstar <brickstar@gmail.com>
2018-03-21 09:26:17 -0600

    testing one letter

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,5 +1,7 @@
 class Caesar
-
+  attr_reader :character_hash
   def initialize
+    @character_hash = {}
   end
+  
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -6,5 +6,15 @@ require 'pry'
 
 class CaesarTest < Minitest::Test
 
-  
+  def test_it_exists
+    cc = Caesar.new
+
+    assert_instance_of Caesar, cc
+  end
+
+  def test_one_letter
+    cc = Caesar.new
+
+    assert_equal "q", cc.one_letter("t")
+  end
 end
