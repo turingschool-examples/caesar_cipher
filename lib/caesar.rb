f0e27a58f33284f1701be4cfc43c0f1492273e4b
JamisonOrdway <jamisonordway@gmail.com>
2018-03-21 10:47:14 -0600

    Iteration 0

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,28 @@
+require './test/caesar_test'
+
 class Caesar
-  attr_reader :key
+
+  attr_accessor :string
+                :number
+                :char_map
+                :letters
 
   def initialize
+    @string = "the quick brown"
+    @number = 3
+    @char_map = ("a".."z").to_a
+    @letters = []
+  end
+
+  def string_splitter(string)
+    @letters << @string.split('')
+  end
+
+  def rotator(string_splitter, number)
+    index = @char_map.index(letter)
+    @rotated = @letters.each do |letter|
+      @char_map.rotate(number)[index]
+    end
+
   end
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,19 @@
-# YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/caesar'
+
+class CaesarTest < Minitest::Test
+
+  def test_it_exists
+    caesar = Caesar.new
+    assert_instance_of Caesar, caesar
+  end
+
+  def test_it_splits_string
+    skip
+    caesar = Caesar.new
+    assert_equal @letters, ['t','h','e',' ','q','u','i','c','k',' ','b','r','o','w','n']
+  end
+
+end
