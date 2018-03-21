bd27a79d3d8e2280270d73798a665e2aeddc8ee3
rcm32000 <rcm32000@yahoo.com>
2018-03-21 10:52:16 -0600

    Refactored

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -19,17 +19,25 @@ class Caesar
       if letter == ' '
         letter
       elsif capitals.include?(letter)
-        lower = letter.downcase
-        index = 0
-        index += 1 until lower == rotation_key(key)[index][0]
-        rotation_key(key)[index][1].upcase
+        rotate_capital(letter, key)
       else
-        index = 0
-        index += 1 until letter == rotation_key(key)[index][0]
-        rotation_key(key)[index][1]
+        rotate(letter, key)
       end
     end
     encrypted.join('')
+  end
+
+  def rotate_capital(letter, key)
+    lower = letter.downcase
+    index = 0
+    index += 1 until lower == rotation_key(key)[index][0]
+    rotation_key(key)[index][1].upcase
+  end
+
+  def rotate(letter, key)
+    index = 0
+    index += 1 until letter == rotation_key(key)[index][0]
+    rotation_key(key)[index][1]
   end
 
   def cipher_to_eng(message, key = 3)
@@ -38,17 +46,25 @@ class Caesar
       if letter == ' '
         letter
       elsif capitals.include?(letter)
-        lower = letter.downcase
-        index = 0
-        index += 1 until lower == unrotate_key(key)[index][0]
-        unrotate_key(key)[index][1].upcase
+        unrotate_capital(letter, key)
       else
-        index = 0
-        index += 1 until letter == unrotate_key(key)[index][0]
-        unrotate_key(key)[index][1]
+        unrotate(letter, key)
       end
     end
     encrypted.join('')
+  end
+
+  def unrotate_capital(letter, key)
+    lower = letter.downcase
+    index = 0
+    index += 1 until lower == unrotate_key(key)[index][0]
+    unrotate_key(key)[index][1].upcase
+  end
+
+  def unrotate(letter, key)
+    index = 0
+    index += 1 until letter == unrotate_key(key)[index][0]
+    unrotate_key(key)[index][1]
   end
 
   def from_file(file)
@@ -56,7 +72,7 @@ class Caesar
     File.open('./lib/' + file).each do |line|
       lines << line
     end
-    message_lines = lines.map {|line| line.chomp}
+    message_lines = lines.map(&:chomp)
     message = message_lines.join
     eng_to_cipher(message)
   end
