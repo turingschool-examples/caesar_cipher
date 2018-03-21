c3e5f489416681d56f9a52d9cdde552b852c63d5
Tyler Westlie <tyler.westlie@gmail.com>
2018-03-21 09:09:02 -0600

    test init

--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,11 @@
 # YOUR TESTS GOES HERE
 # I PITY THE FOOL WHO DOESNT WRITE TESTS
+require'minitest/autorun'
+require'minitest/pride'
+require'./lib/caesar.rb'
+
+class TestCaesar < Minitest::Test
+  def test_init
+    
+  end
+end
