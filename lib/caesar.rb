1b4bcb1750bf54c667c9c30768479bef9ab66c3a
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-21 10:55:57 -0600

    continue method testing for to_english method. could not get to work

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -10,13 +10,19 @@ class Caesar
     @shift = shift
     cipher = alphabet.chars.rotate(@shift += 20).join
     return string.swapcase.tr(alphabet, cipher) if cipher.include?(('A'..'Z').to_a.join)
-    string.tr(@alphabet, cipher)
+    string.tr(alphabet, cipher)
   end
 
   def from_file(filename)
     File.open(filename).each do |line|
       @line = line.strip.to_s
     end
-    to_cipher(@line, @shift + 3)
+    to_cipher(@line, shift + 3)
+  end
+
+  def to_english(string)
+    binding.pry
+    cipher = alphabet.chars.rotate(@shift - 5).join
+    string.tr(cipher, alphabet)
   end
 end
