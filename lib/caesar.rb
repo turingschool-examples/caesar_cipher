1df6b504a8fbfa10d2b06e4b1f6b32019c212071
Jane Kim <Janeekim12@gmail.com>
2018-03-21 11:01:09 -0600

    final

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,81 @@
 class Caesar
-  attr_reader :key
+  attr_reader :dictionary
 
   def initialize
+    @dictionary = {"a" => "x",
+                    "b" => "y",
+                    "c" => "z",
+                    "d" => "a",
+                    "e" => "b",
+                    "f" => "c",
+                    "g" => "d",
+                    "h" => "e",
+                    "i" => "f",
+                    "j" => "g",
+                    "k" => "h",
+                    "l" => "i",
+                    "m" => "j",
+                    "n" => "k",
+                    "o" => "l",
+                    "p" => "m",
+                    "q" => "n",
+                    "r" => "o",
+                    "s" => "p",
+                    "t" => "q",
+                    "u" => "r",
+                    "v" => "s",
+                    "w" => "t",
+                    "x" => "u",
+                    "y" => "v",
+                    "z" => "w",
+                    " " => " ",
+                    "A" => "X",
+                    "B" => "Y",
+                    "C" => "Z",
+                    "D" => "A",
+                    "E" => "B",
+                    "F" => "C",
+                    "G" => "D",
+                    "H" => "E",
+                    "I" => "F",
+                    "J" => "G",
+                    "K" => "H",
+                    "L" => "I",
+                    "M" => "J",
+                    "N" => "K",
+                    "O" => "L",
+                    "P" => "M",
+                    "Q" => "N",
+                    "R" => "O",
+                    "S" => "P",
+                    "T" => "Q",
+                    "U" => "R",
+                    "V" => "S",
+                    "W" => "T",
+                    "X" => "U",
+                    "Y" => "V",
+                    "Z" => "W",}
   end
+
+  def eng_to_cipher(words, num)
+    array_of_words = words.split("")
+    cipher_chars = array_of_words.map do |char|
+    @dictionary[char]
+    end
+    cipher_chars.join
+  end
+
+  def from_file
+    File.open("input.txt").each do |line|
+    end
+  end
+
+  def cipher_to_eng(words)
+    array_of_words = words.split("")
+    eng_chars = array_of_words.map do |key, hash|
+      @dictionary[]
+    end
+  eng_chars.join
+  end
+
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,34 @@
-# YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+require 'minitest/autorun'
+require 'minitest/pride'
+require'./lib/caesar'
+require 'pry'
+
+class CaesarTest < Minitest::Test
+  attr_reader :caesar
+  def setup
+    @caesar = Caesar.new
+  end
+
+  def test_it_exists
+    assert_instance_of Caesar, caesar
+  end
+
+  def test_it_can_translate_english_to_cipher
+    assert_equal "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald", caesar.eng_to_cipher("the quick brown fox jumps over the lazy dog", 3)
+  end
+
+  def test_it_is_case_sensitive
+    assert_equal "Qeb nrfzh yoltk clu GRJMP lsbo qeb ixwv ALD",
+    caesar.eng_to_cipher("The quick brown fox JUMPS over the lazy DOG", 3)
+  end
+
+  def test_it_can_take_text_from_a_file
+    
+    assert_equal "F xj fk x cfib ", caesar.from_file("input.txt")
+  end
+
+  def test_it_can_translate_cipher_to_english
+    assert_equal "the quick brown fox jumps over the lazy dog",  caesar.cipher_to_eng("qeb nrfzh yoltk clu grkmp lsbo qeb ixwv ald")
+  end
+
+end
