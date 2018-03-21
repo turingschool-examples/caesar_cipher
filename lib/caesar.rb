efc242c92723d2bb53dc7de658f230f16f808613
Judson Stevens <Judson.Stevens2gmail.com>
2018-03-21 10:02:59 -0600

    Final changes to timed individual assesment practice

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -3,22 +3,30 @@ class Caesar
   def eng_to_cipher(phrase, count)
     new_phrase = phrase.chars.map do |letter|
       if letter == letter.upcase
-        letters = ("A".."Z").to_a.join
-        new_letters = letters.chars.rotate(-count).join
-        new_letter = letter.tr(letters, new_letters)
+        upper_case_transform(letter, count)
       else 
-        letters = ("a".."z").to_a.join
-        new_letters = letters.chars.rotate(-count).join
-        new_letter = letter.tr(letters, new_letters)
+        lower_case_transform(letter, count)
       end
     end
     puts new_phrase.join
   end
 
-  def from_file(input_file)
+  def upper_case_transform(letter, count)
+    letters = ("A".."Z").to_a.join
+    new_letters = letters.chars.rotate(-count).join
+    new_letter = letter.tr(letters, new_letters)
+  end
+
+  def lower_case_transform(letter, count)
+    letters = ("a".."z").to_a.join
+    new_letters = letters.chars.rotate(-count).join
+    new_letter = letter.tr(letters, new_letters)
+  end
+
+  def from_file(input_file, count)
     input_array = File.readlines(input_file)
     input_array.each do |line|
-      eng_to_cipher(line, 3)
+      eng_to_cipher(line, count)
     end
   end
 
@@ -39,4 +47,7 @@ class Caesar
 
 end
 
-Caesar.new.from_file("example.txt")
+a = Caesar.new
+a.from_file("example.txt", 4)
+a.cipher_to_eng("F xj fk x cfib", 3 )
+
