12914f55546a9739a0ebae8b145ab7ace23dc9ab
Manoj Panta <manojpanta95@gmail.com>
2018-03-21 09:07:32 -0600

    initial commit

--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,23 @@
+
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/caesar'
+
+class CaesarTest < Minitest::Test
+
+  def test_if_it_exists
+    caesar = Caesar.new
+    assert_instance_of Caesar, caesar
+
+  end
+
+
+
+
+
+
+
+end
 # YOUR TESTS GOES HERE
 # I PITY THE FOOL WHO DOESNT WRITE TESTS
