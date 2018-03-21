f7edbcc4d384a100b232d318a4ae1e97b8415d74
Tyler Westlie <tyler.westlie@gmail.com>
2018-03-21 09:29:30 -0600

    added convert_phrase

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,7 +1,16 @@
 class Caesar
-  attr_reader :key
+  attr_reader :phrase
 
-  def initialize
-
+  def initialize(phrase)
+    @phrase = phrase
   end
+
+  def make_key
+    
+  end
+
+  def convert_phrase(phrase)
+    new_phrase = phrase.split ('')
+  end
+
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -5,7 +5,16 @@ require'minitest/pride'
 require'./lib/caesar.rb'
 
 class TestCaesar < Minitest::Test
+
   def test_init
-    
+    caesar = Caesar.new("Hello")
+
+    assert_instance_of Caesar, caesar
   end
+
+  def test_convert_phrase
+    caesar = Caesar.new("Hello")
+    assert_equal ["H","e","l","l","o"], caesar.convert_phrase("Hello")
+  end
+
 end
