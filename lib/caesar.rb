3d279c50a7bd6c2255cf2aa3a66bca97e8a251b0
colehart <36777482+colehart@users.noreply.github.com>
2018-03-21 10:55:42 -0600

    Add files via upload
    incomplete Iteration 0

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,41 @@
 class Caesar
-  attr_reader :key
+  attr_reader :code,
+              :alphabet
 
   def initialize
+    @alphabet = ("a".."z").to_a
+      # + ('A'..'Z').to_a.join) - iteration 1
+    @code = []
   end
+
+  def eng_to_cipher(msg_to_be_encoded, amt_shift)
+    amt_shift.-@
+    ascii_msg = msg_to_be_encoded.chars.map(&:ord)
+    #ordinates each char in array to ascii number
+
+    # shift_it = @alphabet.size - amt_shift
+    # #scalable shift to include different sizes of alphabets and glyphs.
+
+    shifted_msg = ascii_msg.map do |character|
+      character + 
+    end
+    #shifts message left according to passed amt_shift
+
+  #  @code = shifted.map(&:chr).join
+    #turns ordinated characters back to characters and rejoins to string
+
+  end
+
 end
+
+
+=begin
+Snippets:
+[alphabet.zip(alphabet.rotate(shift))]
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+
+@alphabet = (("a".."z").to_a.join + ('A'..'Z').to_a.join)
+@cipher = alphabet.chars.rotate(shift).join
+
+Want to use subarrays for words to keep space integrity
+=end
