00a1ada5079a74ab7f40c2ee00c3b4733a498f21
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-21 10:53:53 -0600

    First attempt at cipher

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,20 @@
-class Caesar
-  attr_reader :key
 
-  def initialize
-  end
+require 'pry'
+class Caesar
+  attr_reader :text, :number
+
+   def initialize(text, number)
+     @text = text
+     @number = number
+   end
+
+  def eng_to_cipher(text, number)
+   @number.to_i
+   @text.to_s
+   alphabet = ('a'..'z').to_a
+   key = Hash[alphabet.zip(alphabet.rotate(@number))]
+   @text.split do |key, letter| letter + key
+   end
+
+
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,27 @@
-# YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/caesar'
+require 'pry'
+
+class CaesarTest < Minitest::Test
+
+  def test_caeser_exists
+    caesar = Caesar.new("The quick", 3)
+    assert_instance_of Caesar, caesar
+  end
+
+  def test_eng_to_cipher
+    caesar = Caesar.new("The quick", 3)
+    assert_equal "qeb nrfzh", caesar.eng_to_cipher("The quick", 3)
+  end
+
+  def test_file_to_cipher
+    skip
+  end
+
+  def test_cipher_to_eng
+    skip
+  end
+
+
+end
