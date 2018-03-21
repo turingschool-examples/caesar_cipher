4153ea7ebb331eb2e03af9a236177d67625443f5
Bryancombs66 <bryancombs@msn.com>
2018-03-21 10:45:10 -0600

    need to figure out my shift

--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,20 @@
-# YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/caesar"
+require "pry"
+
+class CaesarTest < Minitest::Test
+
+  def test_caesar_class_exists
+    caesar = Caesar.new("test",3)
+    assert_equal "test",  caesar.word
+  end
+
+  def test_eng_to_cipher
+    ceasar = Caesar.new("the quick brown fox jumps over the lazy brown
+             dog",3)
+    assert_equal "qeb nrfzh yoltk clu grkmp lsbo qeb ixwv ald",
+                  ceasar.eng_to_cipher
+  end
+
+end
