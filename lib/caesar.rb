eabcd4b7df0ccda6e9dfbc74ad6ed9b6892f4f2b
apoc64 <sschwedt@gmail.com>
2018-03-21 10:36:41 -0600

    working project

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,75 @@
+require "pry"
+
 class Caesar
-  attr_reader :key
+  # attr_reader :key
 
   def initialize
+    @aord = "a".ord #97
+    @capAord = "A".ord #65
+    @zord = "z".ord #122
+    @capZord = "Z".ord #90
   end
-end
+
+  def eng_to_cipher(message, rotation, decipher = false)
+    #need to ignore spaces
+    words = message.split
+    new_words = words.map do |word|
+      cipher_word(word, rotation, decipher)
+    end
+    new_words.join(" ")
+  end #e2c
+
+  def cipher_word(message, rotation, decipher)
+    letters = message.split("")
+    letters.map do |letter|
+      letter_num = letter.ord
+      new_letter_num = letter_num - rotation
+      #needs to avoid punctuation:
+      if decipher
+        new_letter_num = check_for_bad_decipher_characters(new_letter_num)
+      else
+        new_letter_num = check_for_bad_encipher_characters(new_letter_num)
+      end
+      new_letter_num.chr
+    end.join
+  end #cw
+
+  def check_for_bad_encipher_characters(new_letter_num)
+    #avoid punctuation for cipher
+    if new_letter_num < @aord && new_letter_num > @capZord
+      shift = @aord - new_letter_num
+      new_letter_num = @zord - shift + 1
+    elsif new_letter_num < @capAord
+      shitf - @capAord - new_letter_num
+      new_letter_num = @capZord - shift + 1
+    end
+    new_letter_num
+  end
+
+  def check_for_bad_decipher_characters(new_letter_num)
+    #avoid punctuation for decipher:
+    if new_letter_num > @zord
+      shift = @zord - new_letter_num
+      new_letter_num = @aord - shift - 1
+    elsif new_letter_num > @capZord && new_letter_num < @capAord
+      shitf - @capAord - new_letter_num
+      new_letter_num = @capZord - shift - 1
+    end
+    new_letter_num
+  end
+
+  def from_file(file_name)
+    file = File.open(file_name, "r")
+    message = file.read
+    # binding.pry
+    eng_to_cipher(message, 3)
+  end
+
+  def cipher_to_eng(message)
+    eng_to_cipher(message, -3, true)
+  end
+
+end #caesar class
+
+# caesar = Caesar.new
+# binding.pry
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,48 @@
 # YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+# # I PITY THE FOOL WHO DOESNT WRITE TESTS
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/caesar'
+
+class CaesarTest < MiniTest::Test
+# Caesar Cipher
+# Using this repo, build a well tested Ruby program that translates a message using the Caesar Cipher.
+#
+# You can read more about the Caesar Cipher here.
+#
+# Note: We are shifting left. So if we shift 3, A is now X, D is now A and so forth.
+
+# Push your solution to your fork
+# Use Github's interface to create a pull request
+# Iteration 0
+def setup
+  @caesar = Caesar.new
+end
+
+def test_it_can_cipher
+coded = @caesar.eng_to_cipher("the quick brown fox jumps over the lazy dog", 3)
+assert_equal "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald", coded
+end
+
+# Iteration 1
+def test_it_can_handle_cases
+  coded = @caesar.eng_to_cipher("The quick brown fox JUMPS over the lazy DOG", 3)
+  assert_equal "Qeb nrfzh yoltk clu GRJMP lsbo qeb ixwv ALD", coded
+end
+
+# Iteration 2
+# Translate English to ciphertext from a file
+def test_it_can_cipher_a_file
+  file = File.open("input.txt", "w")
+  file.write("I am in a file")
+  file.close
+  assert_equal "F xj fk x cfib",  @caesar.from_file("input.txt")
+end
+
+# Iteration 3
+# Translate ciphertext to English
+def test_it_can_decipher
+  assert_equal "the quick brown fox jumps over the lazy dog", @caesar.cipher_to_eng("qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald")
+end
+
+end #test class
