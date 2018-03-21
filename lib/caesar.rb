af7e05d19a61c8769bc3a4b3642630de3abcdd76
ClaireBvs <clairembeauvais@gmail.com>
2018-03-21 10:58:59 -0600

    caesar

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,25 @@
 class Caesar
-  attr_reader :key
 
-  def initialize
+  attr_reader :key, :eng_to_cipher
+
+  def initialize(eng_to_cipher)
+    @eng_to_cipher = eng_to_cipher
+    # @left = 3
   end
+
+  def eng_to_cipher
+   @eng_to_cipher = "the quick brown fox jumps over the lazy dog"
+  # @left = 3
+  end
+
+  def split
+    @eng_to_cipher = "the quick brown fox jumps over the lazy dog"
+    a.split("")
+  end
+
+  def rotate
+    @eng_to_cipher.rotate(-3)
+    eng_to_cipher = -3
+  end
+
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,32 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/caesar.rb'
+
+class CaesarTest < MiniTest::Test
+
+  def test_it_exits
+    caesar = Caesar.new("the quick brown fox jumps over the lazy dog")
+    assert_instance_of Caesar, caesar
+  end
+
+  def test_translate_english_to_ciphertext
+    caesar = Caesar.new("the quick brown fox jumps over the lazy dog")
+    assert_equal "the quick brown fox jumps over the lazy dog", caesar.eng_to_cipher
+  end
+
+  def test_it_convert_string_to_array
+    caesar = Caesar.new("the quick brown fox jumps over the lazy dog")
+    assert_equal [], caesar.split
+
+  end
+
+  def test_translate_shift3
+    caesar = Caesar.new
+    assert_equal "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald", caesar.eng_to_cipher.split.rotate
+  end
+
+end
+
+
 # YOUR TESTS GOES HERE
 # I PITY THE FOOL WHO DOESNT WRITE TESTS
